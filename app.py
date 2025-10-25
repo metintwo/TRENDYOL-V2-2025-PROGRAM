853ea4ac46e92cb8924fd550e7290404b66d1c6b
metintwo <ozlp97@gmail.com>
2025-10-25 13:10:38 +0300

    v7.2.1.1

--- app.py
+++ app.py
@@ -637,4 +637,4 @@ def etiket_yazdir(supplier_id, package_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
