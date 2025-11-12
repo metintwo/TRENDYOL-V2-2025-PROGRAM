e2c51c7545e854e59733c427f156e2481f4cce08
metintwo <ozlp97@gmail.com>
2025-11-12 16:53:28 +0300

    v7.3.1

--- app.py
+++ app.py
@@ -937,7 +937,6 @@ def kargo_toplama():
 
 # ---- Main ----
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
 
